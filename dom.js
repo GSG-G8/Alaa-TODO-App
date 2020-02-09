2765a29ca990f4736ac65e1b91b0a4af2c2f52a4
“Alaa <alaa.swaireh@gmail.com>
2020-02-09 22:41:18 +0200

    add dom file relates #5

---
+++ dom.js
@@ -0,0 +1,75 @@
+// part 2 linking it all together
+// The function here is called an iife,
+// it keeps everything inside hidden from the rest of our application
+(function() {
+    // This is the dom node where we will keep our todo
+    var container = document.getElementById('todo-container');
+    var addTodoForm = document.getElementById('myList');
+  
+    var state = [
+      { id: -3, description: 'first todo' },
+      { id: -2, description: 'second todo' },
+      { id: -1, description: 'third todo' },
+    ]; // this is our initial todoList
+  
+    // This function takes a todo, it returns the DOM node representing that todo
+    var createTodoNode = function(todo) {
+      var todoNode = document.createElement('li');
+      // you will need to use addEventListener
+  
+      // add span holding description
+      let todoSpan = document.createElement('span');
+      todoSpan.textContent = todo.description;
+      todoNode.appendChild(todoSpan);
+  
+      // this adds the delete button
+      var deleteButtonNode = document.createElement('button');
+      deleteButtonNode.textContent = 'X';
+      deleteButtonNode.classList.add("deleteBtn");
+
+      deleteButtonNode.addEventListener('click', function(event) {
+        var newState = todoFunctions.deleteTodo(state, todo.id);
+        update(newState);
+      });
+      todoNode.appendChild(deleteButtonNode);
+  
+      // add markTodo button
+  
+      // add classes for css
+  
+      return todoNode;
+    };
+  
+    // bind create todo form
+    if (addTodoForm) {
+      addTodoForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        let description = event.target.querySelector(".title").value
+  
+        // hint: todoFunctions.addTodo
+        var newState = todoFunctions.addTodo(state, description);
+        update(newState);
+        document.getElementById("item").value = "";
+      });
+    }
+  
+    // you should not need to change this function
+    var update = function(newState) {
+      state = newState;
+      renderState(state);
+    };
+  
+    // you do not need to change this function
+    var renderState = function(state) {
+      var todoListNode = document.createElement('ul');
+  
+      state.forEach(function(todo) {
+        todoListNode.appendChild(createTodoNode(todo));
+      });
+  
+      // you may want to add a class for css
+      container.replaceChild(todoListNode, container.firstChild);
+    };
+  
+    if (container) renderState(state);
+  })();
